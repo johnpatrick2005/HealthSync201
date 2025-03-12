62243a1aa115caed471813df5a13be1ebd467fe5
tristanR12 <trosel@csmbataan.ph.education>
2025-03-12 18:28:59 +0800

    Models

--- HealthSync.Models/User.cs
+++ HealthSync.Models/User.cs
@@ -3,9 +3,13 @@
     public class User
     {
         public Guid Id { get; set; }
+
+        public Guid UserRole { get; set; }
         public string? EmailAddress { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+
+
 
     }
 }
